4fb34ad367acba6b9b8ac6200c67acb04bec7aa5
Andrew Chitester <thenightmail@proton.me>
2025-09-01 20:11:11 -0400

    init

---
+++ build-site.el
@@ -0,0 +1,41 @@
+;; Load the publishing system
+(require 'ox-publish)
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "my-org-site"
+             :recursive t
+             :base-directory "./content"
+             :publishing-directory "./public"
+             :with-author nil
+             :with-toc nil
+             :section-numbers nil
+             :time-stamp-file nil
+             :publishing-function 'org-html-publish-to-html)))
+
+;; Customize the HTML output
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style t)
+
+
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete")
